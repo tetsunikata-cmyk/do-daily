af8b3aea29e0d6842955bc0de87d1377e084dc45
tetsunikata-cmyk <tetsunikata@gmail.com>
2025-12-09 18:30:20 +0900

    Streak（連続日数）を出す

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -2,4 +2,20 @@ class Habit < ApplicationRecord
   belongs_to :user
  
   has_many :habit_logs, dependent: :destroy
+
+  def current_streak
+    streak = 0
+    day = Date.current
+
+    loop do
+      if habit_logs.exists?(done_on: day)
+        streak += 1
+        day -= 1
+      else
+        break
+      end
+    end
+
+    streak
+  end
 end
