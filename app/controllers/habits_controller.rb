23934f01d971aa4f330d9a9771e6b64d288a5c54
tetsunikata-cmyk <tetsunikata@gmail.com>
2025-12-09 18:15:01 +0900

    コントローラ

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,19 +1,47 @@
 class HabitsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_habit, only: [:edit, :update, :destroy]
+
   def index
+    @habits = current_user.habits
   end
 
   def new
+    @habit = current_user.habits.new
   end
 
   def create
+    @habit = current_user.habits.new(habit_params)
+    if @habit.save
+      redirect_to habits_path, notice: "Habit created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @habit.update(habit_params)
+      redirect_to habits_path, notice: "Habit updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @habit.destroy
+    redirect_to habits_path, notice: "Habit deleted."
+  end
+
+  private
+
+  def set_habit
+    @habit = current_user.habits.find(params[:id])
+  end
+
+  def habit_params
+    params.require(:habit).permit(:title, :description)
   end
 end
