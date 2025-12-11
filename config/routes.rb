d249bfa8b762328110d69b2c66d4bb93b017ef5b
tetsunikata-cmyk <tetsunikata@gmail.com>
2025-12-11 11:08:41 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'roadmaps/show'
-  get 'goals/show'
   devise_for :users
 
   # 未ログイン用トップページ
   root "pages#home"
-  get   "roadmap", to: "roadmaps#show"
-  patch "roadmap", to: "roadmaps#update"
 
   # ログイン後のハブ（MyPage）
   get   "mypage", to: "dashboards#show"
@@ -15,7 +11,17 @@ Rails.application.routes.draw do
   # 目標実現ページ
   get "goal", to: "goals#show"
 
+  # 道しるべページ
+  get   "roadmap", to: "roadmaps#show"
+  patch "roadmap", to: "roadmaps#update"
+
+  # 振り返り（テキスト）ページ
+  get   "reviews",     to: "reflections#index"
+  post  "reviews",     to: "reflections#create"
+  patch "reviews/:id", to: "reflections#update", as: :review
+
+  # 習慣＋今日の実行ログ
   resources :habits do
-  resource :habit_log, only: [:create, :destroy]
+    resource :habit_log, only: [:create, :destroy]
   end
 end
