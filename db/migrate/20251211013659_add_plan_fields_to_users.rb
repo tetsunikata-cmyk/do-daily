150a94833c42ab7dde96c636818d2cea80c43963
tetsunikata-cmyk <tetsunikata@gmail.com>
2025-12-11 10:57:29 +0900

    カラム修正

--- db/migrate/20251211013659_add_plan_fields_to_users.rb
+++ db/migrate/20251211013659_add_plan_fields_to_users.rb
@@ -1,7 +1,7 @@
 class AddPlanFieldsToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :vision_text, :string
-    add_column :users, :roadmap_text, :text
-    add_column :users, :annual_schedule_text, :text
+    add_column :users, :vision_text, :string, if_not_exists: true
+    add_column :users, :roadmap_text, :text, if_not_exists: true
+    add_column :users, :annual_schedule_text, :text, if_not_exists: true
   end
 end
