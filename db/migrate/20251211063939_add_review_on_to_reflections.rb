6180591685880c3283527f2512a5da51b118f53c
tetsunikata-cmyk <tetsunikata@gmail.com>
2025-12-18 07:23:57 +0900

    migrationにガード入れる

--- db/migrate/20251211063939_add_review_on_to_reflections.rb
+++ db/migrate/20251211063939_add_review_on_to_reflections.rb
@@ -1,10 +1,7 @@
 class AddReviewOnToReflections < ActiveRecord::Migration[7.1]
   def change
-    # 既にカラムがあれば二重追加で落ちるので、保険で条件付きにしてもOKだけど
-    # 普通はこれで十分
-    add_column :reflections, :review_on, :date
+    return if column_exists?(:reflections, :review_on)
 
-    # 同じ user が同じ日付を複数持たない想定なのでインデックス付けとくと吉
-    add_index :reflections, [:user_id, :review_on], unique: true
+    add_column :reflections, :review_on, :date
   end
 end
