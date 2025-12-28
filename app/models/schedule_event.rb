389a5bab8ca9a379353317958d859528cec00796
tetsunikata-cmyk <tetsunikata@gmail.com>
2025-12-29 07:20:43 +0900

    バリデーション設定

--- app/models/schedule_event.rb
+++ app/models/schedule_event.rb
@@ -1,3 +1,16 @@
 class ScheduleEvent < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 60 }
+  validates :location, presence: true, length: { maximum: 60 }
+  validates :starts_at, presence: true
+
+  validate :ends_after_starts
+
+  private
+
+  def ends_after_starts
+    return if ends_at.blank? || starts_at.blank?
+    errors.add(:ends_at, "は開始より後にしてください") if ends_at <= starts_at
+  end
 end
